ee0fba66ce9658ba24c7ed5b5501bba3b369b904
MrPancakes39 <salmannhassan39@gmail.com>
2022-08-04 13:29:21 +0300

    Added Create String functions

--- string_type/string_type.c
+++ string_type/string_type.c
@@ -1,7 +1,27 @@
-#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 #include "string_type.h"
 
-void test()
+/**
+ * Creates a String from a char-array and it's length.
+ * @param[in] data the c-string.
+ * @param[in] length the length of the string.
+ * @return a String object.
+ */
+String String_from_parts(const char *data, size_t length)
 {
-    printf("Hello Test!\n");
+    String s;
+    s.data = data;
+    s.length = length;
+    return s;
+}
+
+/**
+ * Creates a String from a c-string.
+ * @param[in] cstr a c-string.
+ * @return a String object.
+ */
+String String_from(const char *cstr)
+{
+    return String_from_parts(cstr, strlen(cstr));
 }
--- string_type/string_type.h
+++ string_type/string_type.h
@@ -1,6 +1,22 @@
 #ifndef STRING_TYPE_H_INCLUDED
 #define STRING_TYPE_H_INCLUDED
 
-void test();
+/**
+ * Defines a String type.
+ */
+typedef struct
+{
+    const char *data;
+    size_t length;
+} String;
+
+String String_from_parts(const char *data, size_t length);
+String String_from(const char *cstr);
+
+///< Defines macro for creating an empty string.
+#define String_Empty String_from_parts(NULL, 0)
+
+#define STR_FMT "\"%.*s\""                   ///< Defines macro for printf.
+#define STR_ARG(s) (int)(s).length, (s).data ///< Defines macro for printf.
 
 #endif
